118422a244d5f72a75496479a4d65c199d32833b
Clément Correia <clement.correia@avizzeo.com>
2017-11-29 10:40:59 +0100

    ajout des couleurs des rang dans le tableau

--- src/LCSBundle/Controller/JoueurController.php
+++ src/LCSBundle/Controller/JoueurController.php
@@ -9,7 +9,21 @@ class JoueurController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('LCSBundle:Joueur:index.html.twig');
+        $rangs = $this->getDoctrine()->getRepository("LCSBundle:Rang")->findAll();
+        $style = "";
+        foreach ($rangs as $rang) {
+            $style.="
+                .".$rang->getNom()." {
+                    width: 100%;
+                    display: inline-block;
+                    text-align: center;
+                    background-color: ".$rang->getCouleur().";
+                }
+                ";
+        }
+        return $this->render('LCSBundle:Joueur:index.html.twig', array(
+            "style" => $style
+        ));
     }
 
     public function detailsAction($pseudo)
@@ -22,18 +36,18 @@ class JoueurController extends Controller
     }
 
     public function listeAction()
-    {
+    {        
         $joueurs = $this->getDoctrine()->getRepository("LCSBundle:Joueur")->findAll();
         $data     = ['data' => []];
         foreach($joueurs as $joueur) {
-            $color = $joueur ? $joueur->getRang()->getCouleur() : null;
-            $rang = "<span class=\"rang\" style=\"background-color: $color;\">".($joueur ? $joueur->getRang()->getNom() : null)."</span>";
+            $color = $joueur ? $joueur->getRang()->getNom() : null;
+            $rang = "<span class=\"$color\">".($joueur ? $joueur->getRang()->getNom() : null)."</span>";
             $data['data'][] = [
                 'pseudo'     => $joueur ? $joueur->getPseudo() : null,
                 'prenom'     => $joueur ? $joueur->getPrenom() : null,
                 'nom'        => $joueur ? $joueur->getNom() : null,
                 'poste'      => $joueur ? $joueur->getPoste()->getNom() : null,
-                'rang'      => $rang,
+                'rang'       => $rang,
                 //Permet de récupérer l'id pour chaque td du tableau
                 //Pour pouvoir gérer le click qur la ligne en js, et rediriger vers la bonne affaire
                 'DT_RowId'   => 'id_'.$joueur->getPseudo()
